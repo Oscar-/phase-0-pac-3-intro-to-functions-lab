1e0b74101f1c28cf1437a8303667617690777698
Oscar Cabrera <cabreraoscar0521@gmail.com>
2024-06-01 12:35:33 -0500

    tests completed

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+"Hello!".toUpperCase(); // 'HELLO!'
+function whisper(string) {
+    return string.toLowerCase();
+  }
+"Hello!".toLowerCase(); // 'HELLO!'
+function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function logWhisper(string) {
+  console.log(string.toLowerCase());}
+  "Hello!".toLowerCase();
+  function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase() === string) {
+      return "I can't hear you!";
+    }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  if (string === "Let's have dinner together!") {
+  return "I would love to!";
+  }
+}
+
+  
